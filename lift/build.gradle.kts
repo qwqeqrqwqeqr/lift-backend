4b9cb4d1f52517ba4217d4f7b38596f501137631
qwqeqrqwqeqr <rhdtkdxor123@kyonggi.ac.kr>
2022-10-24 23:12:35 +0900

    build : build mockk mockk 라이브러리를 추가합니다.

--- lift/build.gradle.kts
+++ lift/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 
 	testImplementation("io.kotest:kotest-runner-junit5:5.5.1")
 	testImplementation("io.kotest:kotest-assertions-core:5.5.1")
+	testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {
--- lift/src/test/kotlin/com/gradation/lift/repository/UserRepositoryTest.kt
+++ lift/src/test/kotlin/com/gradation/lift/repository/UserRepositoryTest.kt
@@ -1,9 +1,11 @@
 package com.gradation.lift.repository
 
 import com.gradation.lift.config.TestDatabaseConfiguration
+import com.gradation.lift.data.entity.User
 import io.kotest.core.spec.style.FunSpec
 import jdk.internal.vm.vector.VectorSupport.test
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ContextConfiguration(classes = [UserRepository::class, TestDatabaseConfiguration::class])
 class EmployeeRepositoryTest(userRepository: UserRepository) : FunSpec({
@@ -11,8 +13,8 @@ class EmployeeRepositoryTest(userRepository: UserRepository) : FunSpec({
     extension(SpringExtension)
 
     test("직원을 저장할 수 있다.") {
-        val employee = userRepository(name = "test")
-        val saved = employeeRepo.save(employee)
+        val employee = User(id = "test")
+        val saved = userRepository.save(employee)
 
         saved.name shouldBe employee.name
     }
